5f1153a0a4da81c6b43daf8f71541127bd2de94a
Facundo Chan <FacundoChan01@gmail.com>
2025-03-17 15:31:54 +0800

    Add gRPC client connection checks and timeout configuration

--- internal/common/client/grpc.go
+++ internal/common/client/grpc.go
@@ -2,6 +2,9 @@ package client
 
 import (
 	"context"
+	"errors"
+	"net"
+	"time"
 
 	"github.com/FacundoChan/gorder-v1/common/discovery"
 	"github.com/FacundoChan/gorder-v1/common/genproto/orderpb"
@@ -13,6 +16,9 @@ import (
 )
 
 func NewStockGRPCClient(ctx context.Context) (client stockpb.StockServiceClient, close func() error, err error) {
+	if !WaitForStockGRPCClient(viper.GetDuration("dial-grpc-timout") * time.Second) {
+		return nil, func() error { return nil }, errors.New("stock grpc client not ready")
+	}
 	grpcAddr, err := discovery.GetServiceAddr(ctx, viper.GetString("stock.service-name"))
 	if err != nil {
 		return nil, func() error { return nil }, err
@@ -31,6 +37,9 @@ func NewStockGRPCClient(ctx context.Context) (client stockpb.StockServiceClient,
 }
 
 func NewOrderGRPCClient(ctx context.Context) (client orderpb.OrderServiceClient, close func() error, err error) {
+	if !WaitForOrderGRPCClient(viper.GetDuration("dial-grpc-timout") * time.Second) {
+		return nil, func() error { return nil }, errors.New("order grpc client not ready")
+	}
 	grpcAddr, err := discovery.GetServiceAddr(ctx, viper.GetString("order.service-name"))
 	if err != nil {
 		return nil, func() error { return nil }, err
@@ -52,4 +61,44 @@ func grpcDialOpts(_ string) []grpc.DialOption {
 	return []grpc.DialOption{
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 	}
+}
+
+func WaitForOrderGRPCClient(timeout time.Duration) bool {
+	logrus.Infof("waiting for order gRPC client connection in %s", timeout)
+	return waitFor(viper.GetString("order.grpc-addr"), timeout)
+}
+
+func WaitForStockGRPCClient(timeout time.Duration) bool {
+	logrus.Infof("waiting for stock gRPC client connection in %s", timeout)
+	return waitFor(viper.GetString("stock.grpc-addr"), timeout)
+}
+
+func waitFor(addr string, timeout time.Duration) bool {
+	portAvailable := make(chan struct{})
+	timeoutCh := time.After(timeout)
+
+	go func() {
+		for {
+			select {
+			case <-timeoutCh:
+				return
+			default:
+				// continue
+			}
+			_, err := net.Dial("tcp", addr)
+			if err == nil {
+				close(portAvailable)
+			}
+
+			time.Sleep(200 * time.Millisecond)
+		}
+	}()
+
+	select {
+	case <-portAvailable:
+		return true
+	case <-timeoutCh:
+		return false
+	}
+
 }
